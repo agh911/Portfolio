b2d15d6ed22c27768ae583f97524d000150cf2f5
Andreea Ghenea <andreeag@Andreeas-MBP.cust.communityfibre.co.uk>
2024-06-02 18:31:02 +0100

    Added fetching update and data loading state

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,20 +11,21 @@ function App() {
   const [user, setUser] = useState([]);
   const [skills, setSkills] = useState([]);
   const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
-        const userData = await getUser();
+        const [userData, skillsData, projectsData] = await Promise.all([getUser(), getSkills(), getProjects()]);
+
         setUser(userData[0]);
-
-        const skillsData = await getSkills();
         setSkills(skillsData[0].technicalSkills);
-
-        const projectsData = await getProjects();
         setProjects(projectsData[0].projects);
       } catch (error) {
         console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchData();
@@ -44,10 +45,23 @@ function App() {
   };
 
   return (
-    <div className={`${isDarkMode ? 'dark' : 'light'} relative h-screen overflow-x-clip`}>
-      <Header isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
-      <Home user={user} skills={skills} projects={projects} isDarkMode={isDarkMode} />
-      <Footer isDarkMode={isDarkMode} />
+    <div className={`${isDarkMode ? 'dark' : 'light'} bg-customBg relative h-screen overflow-x-clip`}>
+      < Header isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
+      {loading ? (
+        <div className={`${isDarkMode ? 'dark' : 'light'} flex flex-col justify-center bg-customBg text-center h-screen`}>
+          <div className={`${isDarkMode ? 'dark' : 'light'} flex justify-center`}>
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="fill-headline animate-spin h-5 w-5 mr-3"><path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z" /></svg>
+            <h5 className="np-font text-headline">Loading portfolio data...</h5>
+          </div>
+          <p className="text-paragraph mt-5">This might take a few seconds. Thank you for your patience!</p>
+        </div>
+      ) :
+        (
+          <>
+            <Home user={user} skills={skills} projects={projects} isDarkMode={isDarkMode} />
+            <Footer isDarkMode={isDarkMode} />
+          </>
+        )}
       <GradientCursor />
     </div>
   )
